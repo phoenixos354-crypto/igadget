7c76634bae961a87df26af961b12337f15542f53
phoenixos354-crypto <phoenix.os354@gmail.com>
2026-04-22 17:39:28 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
+const { defineConfig } = require("vite");
+const react = require("@vitejs/plugin-react");
+const path = require("path");
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
